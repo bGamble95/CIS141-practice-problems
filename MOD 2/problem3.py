b29dbe4fab339ce3251d2b1992d7b9ee0f7d65bf
bGamble95 <bradlayteegambler@gmail.com>
2025-05-03 17:45:40 -0700

    Update problem3.py

--- MOD 2/problem3.py
+++ MOD 2/problem3.py
@@ -1,1 +1,8 @@
+import math
+side1 = int(input("Enter first side of triangle"))
+side2 = int(input("Enter second side of triangle"))
+side3 = int(input("Enter third side of triangle"))
+semi_pemi = (side1+side2+side3)/2
+heron = math.sqrt(semi_pemi*(semi_pemi-side1)*(semi_pemi-side2)*(semi_pemi-side3))
+print(heron)
 
