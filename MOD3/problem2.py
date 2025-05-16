67a8839510533dd9599771a06344f000c78dc309
bGamble95 <bradlayteegambler@gmail.com>
2025-05-15 21:25:08 -0700

    Create problem2.py

---
+++ MOD3/problem2.py
@@ -0,0 +1,10 @@
+name = input("What is your name?")
+age = int(input("How old are you?"))
+age1 = str(age)
+age2 = str(age + 1)
+
+text1 = "Hello "
+text2 = "! You are "
+text3 = " years old. Next year, you will be "
+text4 = " years old."
+print(text1 + name + text2 + age1 + text3 + age2 + text4)
