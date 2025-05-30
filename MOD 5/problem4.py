611ed623cd0413c3ae537b1e2e15d193db40208e
bGamble95 <bradlayteegambler@gmail.com>
2025-05-29 23:27:05 -0700

    Create problem4.py

---
+++ MOD 5/problem4.py
@@ -0,0 +1,6 @@
+row = 5
+for i in range(1, 6):
+    for j in range(1, 6):
+        print(str(j * i) + "  ", end = "")
+    print("")
+#I have no idea how to straighten out the columns with spacing between the numbers but each row is correct so I hope that counts!
